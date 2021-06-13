c5875de5d477ebc2d1cc3b05da7fc523211d56fa
Sachin Singhal <singhals912@gmail.com>
2021-06-12 20:16:03 -0400

    Update pp_d.py

--- pipeline1/pp_d.py
+++ pipeline1/pp_d.py
@@ -1,56 +1,34 @@
-from google.cloud import storage
 import os
+import io
+from io import BytesIO
 from transformers.pipelines import pipeline
-import csv
-import time
 
-# creating a GCS client
-client = storage.Client()
+hg_BERT = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad",
+                   tokenizer="distilbert-base-uncased-distilled-squad")
+from google.cloud import storage
+import pandas as pd
 
-bucket = client.get_bucket('mgmt-590-class')
+storage_client = storage.Client()
+bucket = storage_client.get_bucket('mgmt-590-class')
 
-for filecsv in client.list_blobs('mgmt-590-class'):
-    blob = bucket.blob(filecsv.name)
-    blob.download_to_filename(os.path.join("/tmp",filecsv.name))
-    #blob.download_to_filename(filecsv.name)
-    models = {"name": "distilled-bert",
-             "tokenizer": "distilbert-base-uncased-distilled-squad",
-             "model": "distilbert-base-uncased-distilled-squad",
-             "pipeline": pipeline('question-answering',
-             model="distilbert-base-uncased-distilled-squad",
-             tokenizer="distilbert-base-uncased-distilled-squad")
-             }
-    final_file = []
-    hg_comp = models['pipeline']
-    answer = []
-    questions = []
-    contexts = []
+files = bucket.list_blobs()
+fileList = [file.name for file in files if '.' in file.name]
 
-    with open(os.path.join("/tmp",filecsv.name), 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            context = row["context"]
-            question = row["question"]
-            answer.append(hg_comp({'question': question, 'context': context})['answer'])
-            questions.append(question)
-            contexts.append(context)
-        final_file.append(questions)
-        final_file.append(contexts)
-        final_file.append(answer)
-        print(final_file)
-    timestamp = str(int(time.time()))
-    try:
-        with open(os.path.join("/pfs/out",filecsv.name.replace(".csv",timestamp+".csv")), 'w') as f:
-            fieldnames = ['question','context','answer']
-            writer = csv.DictWriter(f, fieldnames=fieldnames)
-            writer.writeheader()
-
-            for row in zip(*final_file):
-                writer.writerow({'question': row[0],'context': row[1], 'answer': row[2]})
-
-    except:
-        print("Failed created csv file with answers for "+filecsv.name)
-    else:
-        print("Successfully processed the file "+filecsv.name)
-        print("Deleting csv file "+filecsv.name)
-        blob.delete()
+answer_li = []
+df_final = pd.DataFrame()
+df_new = pd.DataFrame()
+for i in fileList:
+    # print(i)
+    blop = bucket.blob(i)
+    data = blop.download_as_string()
+    df = pd.read_csv(io.BytesIO(data), encoding='utf-8', sep=',')
+    df_final = df_final.append(df, ignore_index=True)
+    for i, row in df.iterrows():
+        context = row['context']
+        question = row['question']
+        answer = hg_BERT({'question': question, 'context': context})['answer']
+        answer_li.append(answer)
+# print(answer_li)
+df_new['context'] = df_final['context']
+df_new['question'] = df_final['question']
+df_new['answer'] = answer_li
