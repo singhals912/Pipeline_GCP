2fdc662a7201d1de207869366cb0f63195f73be7
Sachin Singhal <singhals912@gmail.com>
2021-06-12 20:02:35 -0400

    Update pp_d.py

--- pipeline1/pp_d.py
+++ pipeline1/pp_d.py
@@ -1,72 +1,56 @@
 from google.cloud import storage
 import os
-import pandas as pd
-from transformers import pipeline
-from io import StringIO
+from transformers.pipelines import pipeline
+import csv
+import time
 
-
-#Creating a Google Cloud Storage (GCS) Client
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "assignmenttwo-315021-b4e55c23dce8.json"
-models = {}
+# creating a GCS client
 client = storage.Client()
+
 bucket = client.get_bucket('mgmt-590-class')
-blobs = bucket.list_blobs()
-# Initialize our default model
-models = {
-    "default": "distilled-bert",
-    "models": [
-        {
-            "name": "distilled-bert",
-            "tokenizer": "distilbert-base-uncased-distilled-squad",
-            "model": "distilbert-base-uncased-distilled-squad",
-            "pipeline": pipeline('question-answering',
-                                 model="distilbert-base-uncased-distilled-squad",
-                                 tokenizer="distilbert-base-uncased-distilled-squad")
-        }
-    ]
-}
-def question_answer(df):
-    df.columns = [df.columns[0].lower(), df.columns[1].lower(), df.columns[2].lower()]
-    answers =[]
-    for i in range(len(df)):
-        model_Name =""
-        if 'model' in df.columns:
-            if pd.isna(df['model'][i]):
-                model_Name = models['default']
-            else:
-                model_Name = df['model'][i]
-        else:
-            model_Name = models['default']
-        for m in models['models']:
-            if m['name'] == model_Name:
-                hg_comp = m['pipeline']
-        answer = hg_comp({'question': df['question'][i], 'context': df['context'][i]})['answer']
-        answers.append(answer)
-    df['answer'] = answers
-    return df
 
-def main():
-    df_list = []
-    # #Then do other things
+for filecsv in client.list_blobs('mgmt-590-class'):
+    blob = bucket.blob(filecsv.name)
+    blob.download_to_filename(os.path.join("/tmp",filecsv.name))
+    #blob.download_to_filename(filecsv.name)
+    models = {"name": "distilled-bert",
+             "tokenizer": "distilbert-base-uncased-distilled-squad",
+             "model": "distilbert-base-uncased-distilled-squad",
+             "pipeline": pipeline('question-answering',
+             model="distilbert-base-uncased-distilled-squad",
+             tokenizer="distilbert-base-uncased-distilled-squad")
+             }
+    final_file = []
+    hg_comp = models['pipeline']
+    answer = []
+    questions = []
+    contexts = []
 
-    for blob in blobs:
-        str_test = str(blob.download_as_string(), "utf-8")
-        data = StringIO(str_test)
-        df_test = pd.read_csv(data)
-        print(df_test)
-        df_answers = question_answer(df_test)
-        print(df_answers)
-        df_list.append(df_answers)
-    return df_list
-
-def delete_one_file(filename):
+    with open(os.path.join("/tmp",filecsv.name), 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            context = row["context"]
+            question = row["question"]
+            answer.append(hg_comp({'question': question, 'context': context})['answer'])
+            questions.append(question)
+            contexts.append(context)
+        final_file.append(questions)
+        final_file.append(contexts)
+        final_file.append(answer)
+        print(final_file)
+    timestamp = str(int(time.time()))
     try:
-        blob = bucket.blob(filename)
+        with open(os.path.join("/pfs/out",filecsv.name.replace(".csv",timestamp+".csv")), 'w') as f:
+            fieldnames = ['question','context','answer']
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for row in zip(*final_file):
+                writer.writerow({'question': row[0],'context': row[1], 'answer': row[2]})
+
+    except:
+        print("Failed created csv file with answers for "+filecsv.name)
+    else:
+        print("Successfully processed the file "+filecsv.name)
+        print("Deleting csv file "+filecsv.name)
         blob.delete()
-    except Exception as ex:
-        print("Exception occurred while trying to delete files ",ex)
-        raise ex
-
-
-if __name__ == "__main__":
-    main()
